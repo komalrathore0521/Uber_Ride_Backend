785f7e122e7f882e5c999d79cabd450fab06b81e
Komal Rathore <komalrathore0521@gmail.com>
2025-11-05 01:38:28 +0530

    Update main application file to configure OpenAPI and improve startup settings

--- src/main/java/com/project/uber/Uber/UberApplication.java
+++ src/main/java/com/project/uber/Uber/UberApplication.java
@@ -2,7 +2,18 @@ package com.project.uber.Uber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
+
+
+@SecurityScheme(
+		name = "bearerAuth",
+		type = SecuritySchemeType.HTTP,
+		scheme = "bearer",
+		bearerFormat = "JWT",
+		description = "JWT Authorization header using the Bearer scheme"
+)
 @SpringBootApplication
 public class UberApplication {
 
